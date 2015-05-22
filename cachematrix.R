51b2c01f41116d21453e5efb0e343e36313ce15e
vidalbra <vidal.ferreira@me.com>
2015-05-22 18:57:01 +0300

    R Programming Assignment 2 - file uploading

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - Programming Assignment #2
 
-## Write a short comment describing this function
+## The function creates a distinguish vector that caches the calc of its corresponding inverse
+## It creates an object as a list with set and get functions and Lexical Scoping
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- NULL   ## Inverse is NULL on the onset     
+        ## Assign a new argument to be stored and reset inverse calc 
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+## Create set and get functions for the inverse        
+        setInv <- function(inverse) inv <<- inverse
+        getInv <- function() inv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
